435b5b3afac50ae2697549e1b65601483796c66f
LAIONblack1 <Diallomohamed2946@gmail.com>
2026-05-26 23:18:21 +0000

    Create categories.jsx

---
+++ data/categories.jsx
@@ -0,0 +1,6 @@
+export const categories = [
+  { value: "tous", label: "Tous" },
+  { value: "vêtements", label: "Vêtements" },
+  { value: "électronique", label: "Électronique" },
+  { value: "accessoires", label: "Accessoires" },
+];
