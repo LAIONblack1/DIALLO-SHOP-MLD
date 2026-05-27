4bc6252605c52289a7709546067ede7d8cf750f0
LAIONblack1 <Diallomohamed2946@gmail.com>
2026-05-27 01:28:31 +0000

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <div style={{
+      background: '#020617',
+      color: 'white',
+      minHeight: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column'
+    }}>
+      <h1 style={{
+        fontSize: '60px',
+        color: '#22c55e'
+      }}>
+        Diallo Shop MLD
+      </h1>
+
+      <p>
+        Mon site fonctionne maintenant 🚀
+      </p>
+    </div>
+  )
+}
