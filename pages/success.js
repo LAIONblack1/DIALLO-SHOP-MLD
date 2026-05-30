89416e5f019e264508959f5f9ab156923850d6e6
LAIONblack1 <Diallomohamed2946@gmail.com>
2026-05-30 20:40:57 +0000

    Create success.js

---
+++ pages/success.js
@@ -0,0 +1,18 @@
+export default function Success() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>✅ Paiement réussi</h1>
+
+      <p>Merci pour votre commande 🚀</p>
+    </div>
+  );
+      }
